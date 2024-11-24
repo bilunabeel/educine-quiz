5ca9b59dfcc6913a43168ac5e98cbc1f390968c0
bilunabeel <bilunabeelmv@gmail.com>
2024-11-24 12:42:45 +0530

    add hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import {HashRouter,Route,Routes} from 'react-router-dom'
 import Home from './Home'
 import QuesetionCard from './components/QuesetionCard'
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/qcard' element={<QuesetionCard/>} />
 
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
