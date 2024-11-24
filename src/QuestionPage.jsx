947f621038be840a603ff5e0f5b7def977adba10
bilunabeel <bilunabeelmv@gmail.com>
2024-11-24 17:22:17 +0530

    add score board

--- src/QuestionPage.jsx
+++ src/QuestionPage.jsx
@@ -1,26 +1,79 @@
-import React from 'react'
-import QuestionCard from './components/QuestionCard'
+import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
 const QuestionPage = () => {
+
+const totalBoard = [
+    {letter:'R',score:888},{letter:'I',score:888},{letter:'A',score:888},{letter:'S',score:668},{letter:'E',score:888},{letter:'C',score:888}
+]
+
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center gap-5 p-10">
-    <div className='bg-white shadow-lg py-5 px-10 text-gray-800 font-bold text-3xl flex justify-between items-center gap-5 rounded-3xl'><p>Your Value</p>
-    <div className='bg-red-600 px-5 py-2 rounded-2xl text-white'>866</div>
+        <div className='flex justify-center items-center gap-2 bg-white py-5 rounded-3xl'>
+{totalBoard.map((board)=>(
+    <div className=''>
+        <div className='rounded-t-lg w-10 bg-red-500 text-center font-bold text-white'>{board.letter}</div>
+        <div className='font-bold text-center border border-x-black border-b-black rounded-b-lg'>{board.score}</div>
     </div>
-    
-<QuestionCard/>
+))}
+        </div>
+      {/* <div className="bg-white shadow-lg py-5 text-gray-800 font-bold text-3xl flex flex-col justify-between items-center rounded-3xl">
+        <div className="flex gap-3">
+          <div className="bg-gray-600 w-10 flex justify-center text-lg text-white ">
+            R
+          </div>
+          <div className="bg-gray-600 w-10 flex justify-center text-lg text-white ">
+            I
+          </div>
+          <div className="bg-gray-600 w-10 flex justify-center text-lg text-white ">
+            A
+          </div>
+          <div className="bg-gray-600 w-10 flex justify-center text-lg text-white ">
+            S
+          </div>
+          <div className="bg-gray-600 w-10 flex justify-center text-lg text-white ">
+            E
+          </div>
+          <div className="bg-gray-600 w-10 flex justify-center text-lg text-white ">
+            C
+          </div>
+        </div>
+        <div className="flex gap-3">
+          <div className="border border-b-black border-x-black w-10 flex justify-center text-lg  ">
+            888
+          </div>
+          <div className="border border-b-black border-x-black w-10 flex justify-center text-lg  ">
+            888
+          </div>
+          <div className="border border-b-black border-x-black w-10 flex justify-center text-lg  ">
+            888
+          </div>
+          <div className="border border-b-black border-x-black w-10 flex justify-center text-lg  ">
+            888
+          </div>
+          <div className="border border-b-black border-x-black w-10 flex justify-center text-lg  ">
+            888
+          </div>
+          <div className="border border-b-black border-x-black w-10 flex justify-center text-lg  ">
+            888
+          </div>
+        </div>
+      </div> */}
 
-    <div className="flex gap-5 font-bold text-white text-2xl justify-between">
-    <button className="shadow-lg bg-blue-500 w-full py-3 rounded-3xl">
-        Skip
-      </button>
-      <button className="shadow-lg bg-green-500 py-3 w-full rounded-3xl ">
-        True
-      </button>
-      
+      <QuestionCard />
+
+      <div className="flex gap-5 font-bold text-white text-2xl justify-between">
+        <button className="shadow-lg bg-blue-500 w-full py-3 rounded-3xl">
+          Skip
+        </button>
+        <button className="shadow-lg bg-green-500 py-3 w-full rounded-3xl ">
+          True
+        </button>
+
+      </div>
+
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default QuestionPage
+export default QuestionPage;
--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+
+
 const QuestionCard = () => {
   return (
     
-      <div className=" bg-white shadow-lg text-gray-700 rounded-3xl p-10 textce font-bold text-4xl leading-relaxed">
-        What is your favorite Fruit?
+      <div className=" h-72 bg-white shadow-lg text-gray-700 rounded-3xl p-10 font-bold text-xl leading-normal">
+        ഞാൻ വ്യക്തമായ ആഗ്രഹങ്ങളുള്ള ഒരാളാണ്. എന്റെ ലക്ഷ്യങ്ങൾ ഞാൻ സ്വയം തന്നെയാണ് നിർണ്ണയിക്കുന്നത്‌
+
       </div>
   );
 };
